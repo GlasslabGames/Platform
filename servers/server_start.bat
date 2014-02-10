f5e277aff784e58d91bfcc6b914e607ab9c6ab67
Joe <joe@jstty.com>
2014-02-10 11:31:42 -0800

    moved start servers to servers dir added new services

---
+++ servers/server_start.bat
@@ -0,0 +1,6 @@
+start /wait npm install
+start node app-telemetry-collector.js
+start node app-telemetry-dispatcher.js
+start node app-auth.js
+start node app-auth-validate.js
+start node app-assessment.js
