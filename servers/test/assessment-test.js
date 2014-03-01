f9582afbe548497b1650fdefbca1d14e4246d26a
Michael Mulligan <mike.p.mulligan@gmail.com>
2014-02-28 17:20:30 -0800

    added initial assessment sever test, removed test notes

--- servers/test/assessment-test.js
+++ servers/test/assessment-test.js
@@ -1,41 +1,15 @@
 /**
  * Assessment - App Server
  */
-var Assessment    = require('../lib/assessment.js'),
-    ConfigManager = require('../lib/config.manager.js'),
-    config        = new ConfigManager(),
-    should        = require('chai').should();
+var Assessment = require('../app-assessment.js'),
+    should     = require('chai').should();
 
-// testnote - load settings (refactor sep pkg?), verify returns expected options
-var config = new ConfigManager();
-// load config files from first to last until successful
-var options = config.loadSync([
-    "../config.json",
-    "~/hydra.config.json"
-]);
-global.ENV = options.env || 'dev';
-
-// testnote - run & log, verify that it ran "properly"
-var a = new Assessment.Server(options);
-console.log("---------------------------------------------");
-console.log("-- Assessment App Server - Start");
-console.log("---------------------------------------------");
-
-// testnote - catch unexpected err, verify function does log errors
-process.on('uncaughtException', function(err) {
-    console.error("Assessment: Uncaught Error -", err, ", stack:", err.stack);
-});
-
-
-/* 
-Test 1:
-Sort of copy-paste, consider refactoring "../app-*.js" files to allow 
-testing endpoints.
-*/
-describe("Assessment Server", function () {
-    describe("#sever_start()", function () {
-        it("should start without error", function () {
-            a.should.exist;
+// Test 1 : Assessment Server
+describe('Assessment Server', function () {
+    describe('Launch server', function() {
+        it('should launch without error', function (done) {
+            Assessment.should.be.ok;
+            done();
         });
     });
 });
