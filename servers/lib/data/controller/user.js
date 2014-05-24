b09a86edb770896705e03fabd4341f2493853258
Joe <joe@jstty.com>
2014-05-23 22:27:12 -0700

    added user pref save/get

--- servers/lib/data/controller/user.js
+++ servers/lib/data/controller/user.js
@@ -1,73 +1,25 @@
 
-var path      = require('path');
 var _         = require('lodash');
 var when      = require('when');
 var lConst    = require('../../lms/lms.const.js');
 var Util      = require('../../core/util.js');
 
 module.exports = {
-    saveGameData: saveGameData,
-    getGameData:  getGameData,
-    updateDevice: updateDevice
+    saveUserPref: saveUserPref,
+    getUserPref:  getUserPref
 };
 var exampleIn = {};
 var exampleOut = {};
 
-
 // ----------------------------------------
-// game AA, episode 1
-exampleIn.saveGameData = {
+//
+exampleIn.saveUserPref = {
     id: 'AA-1'
 };
-function saveGameData(req, res, next)
+function saveUserPref(req, res, next)
 {
     if( !req.body ) {
-        this.requestUtil.errorResponse(res, { status: "error", error: "Game data missing", key: "missing.data"});
-        return;
-    }
-
-    var userId;
-    if(req.session &&
-       req.session.passport &&
-       req.session.passport.user &&
-       req.session.passport.user.id) {
-        userId = req.session.passport.user.id;
-    } else {
-        this.requestUtil.errorResponse(res, { status: "error", error: "not logged in", key: "invalid.access"});
-        return;
-    }
-
-    if( !( req.params &&
-        req.params.hasOwnProperty("id") ) ) {
-        this.requestUtil.errorResponse(res, {error: "missing client/game Id"});
-        return
-    }
-    var gameId = req.params.id;
-
-    var data = req.body;
-    try{
-        data = JSON.parse(data);
-    } catch(err){
-        // this is ok, data doesn't have to be json
-    }
-
-    this.cbds.saveGameData(userId, gameId, data)
-        .then(function(){
-            this.requestUtil.jsonResponse(res, { status: "ok" });
-        }.bind(this))
-        .then(null, function(err){
-            this.requestUtil.errorResponse(res, err);
-        }.bind(this));
-}
-
-// game AA, episode 1
-exampleIn.getGameData = {
-    id: 'AA-1'
-};
-function getGameData(req, res, next)
-{
-    if( !req.body ) {
-        this.requestUtil.errorResponse(res, { status: "error", error: "Game data missing", key: "missing.data"});
+        this.requestUtil.errorResponse(res, { status: "error", error: "User Preference data missing", key: "missing.data"});
         return;
     }
 
@@ -96,7 +48,7 @@ function getGameData(req, res, next)
         // this is ok, data doesn't have to be json
     }
 
-    this.cbds.getGameData(userId, gameId)
+    this.cbds.saveUserPref(userId, gameId, data)
         .then(function(){
             this.requestUtil.jsonResponse(res, { status: "ok" });
         }.bind(this))
@@ -105,47 +57,47 @@ function getGameData(req, res, next)
         }.bind(this));
 }
 
-
-// ----------------------------------------
-exampleIn.updateDevice = {
-    deviceId: "ASD-QWER-ASD",
-    gameId: "AA-1"
+// game AA, episode 1
+exampleIn.getUserPref = {
+    id: 'AA-1'
 };
-function updateDevice(req, res, next) {
-    if( req.session &&
-        req.session.passport &&
-        req.session.passport.user) {
-
-        var userData = req.session.passport.user;
-
-        // deviveId required
-        if( !(req.body &&
-              req.body.deviceId &&
-              req.body.deviceId.length) ) {
-            this.requestUtil.errorResponse(res, "missing deviceId");
-            return;
-        }
-
-        if( !(req.body &&
-            req.body.gameId &&
-            req.body.gameId.length) ) {
-            this.requestUtil.errorResponse(res, "missing gameId");
-            return;
-        }
-
-        // update device Id
-        //console.log("deviceId:", req.body.deviceId);
-        this.cbds.updateUserDeviceId(userData.id, req.body.gameId, req.body.deviceId)
-            .then(function(){
-                this.requestUtil.jsonResponse(res, { status: "ok" } );
-            }.bind(this))
-
-            // catch all errors
-            .then(null, function(err){
-                this.requestUtil.errorResponse(res, err);
-            }.bind(this));
-
-    } else {
-        this.requestUtil.errorResponse(res, "not logged in");
+function getUserPref(req, res, next)
+{
+    if( !req.body ) {
+        this.requestUtil.errorResponse(res, { status: "error", error: "User Preference data missing", key: "missing.data"});
+        return;
     }
+
+    var userId;
+    if(req.session &&
+        req.session.passport &&
+        req.session.passport.user &&
+        req.session.passport.user.id) {
+        userId = req.session.passport.user.id;
+    } else {
+        this.requestUtil.errorResponse(res, { status: "error", error: "not logged in", key: "invalid.access"});
+        return;
+    }
+
+    if( !( req.params &&
+        req.params.hasOwnProperty("id") ) ) {
+        this.requestUtil.errorResponse(res, {error: "missing client/game Id"});
+        return
+    }
+    var gameId = req.params.id;
+
+    var data = req.body;
+    try{
+        data = JSON.parse(data);
+    } catch(err){
+        // this is ok, data doesn't have to be json
+    }
+
+    this.cbds.getUserPref(userId, gameId)
+        .then(function(){
+            this.requestUtil.jsonResponse(res, { status: "ok" });
+        }.bind(this))
+        .then(null, function(err){
+            this.requestUtil.errorResponse(res, err);
+        }.bind(this));
 }
