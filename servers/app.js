e7a2a3ab7a5f52daeafd575f41bc6af61e5281c9
Joe <joe@jstty.com>
2014-05-25 13:47:46 -0700

    added custom config file loading from app file

--- servers/app.js
+++ servers/app.js
@@ -6,7 +6,7 @@ var Auth           = require('./lib/auth/auth.js');
 var LMS            = require('./lib/lms/lms.js');
 var Data           = require('./lib/data/data.js');
 var Dash           = require('./lib/dash/dash.js');
-var manager = new ServiceManager();
+var manager = new ServiceManager("~/hydra.config.json");
 
 // add all services
 manager.add( Auth );
