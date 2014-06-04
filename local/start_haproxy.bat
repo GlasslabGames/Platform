c0d8e7fc5479f63872ef38610e1648a96197813c
Joe <joe@jstty.com>
2014-06-04 13:25:10 -0700

    update readme's added init sql files removed old scripts fixed problem in playInfo api

--- local/start_haproxy.bat
+++
@@ -1,2 +0,0 @@
-copy haproxy.cfg c:\cygwin\home
-c:\cygwin\bin\bash.exe -l -c "/usr/local/sbin/haproxy.exe -f /home/haproxy.cfg"
--- local/start_haproxy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-screen -d -m -S haproxy haproxy -f ./haproxy.cfg
