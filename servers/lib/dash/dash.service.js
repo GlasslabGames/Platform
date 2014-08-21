ac251250c1f0a07384d4818b7eaaa8573f07d90d
Joe <joe@jstty.com>
2014-08-20 21:23:33 -0700

    refactoring achivements

--- servers/lib/dash/dash.service.js
+++ servers/lib/dash/dash.service.js
@@ -42,7 +42,7 @@ function DashService(options){
         this.lmsStore    = new LmsStore(this.options.lms.datastore.mysql);
         this.dashStore   = new DashStore(this.options.webapp.datastore.mysql);
 
-        this.games = {};
+        this._games = {};
 
     } catch(err){
         console.trace("DashService: Error -", err);
@@ -78,85 +78,102 @@ return when.promise(function(resolve, reject) {
 // returns a uppercase list of all game Ids, game Ids are ALWAYS uppercase
 DashService.prototype.getListOfGameIds = function() {
     var gameIds = [];
-    for(var g in this.games) {
-        if( this.games[g].info &&
-            this.games[g].info.basic &&
-            this.games[g].info.basic.gameId) {
-            gameIds.push( this.games[g].info.basic.gameId.toUpperCase() );
+    for(var g in this._games) {
+        if( this._games[g].info &&
+            this._games[g].info.basic &&
+            this._games[g].info.basic.gameId) {
+            gameIds.push( this._games[g].info.basic.gameId.toUpperCase() );
         }
     }
     return gameIds;
 };
 
 // TODO: replace this with DB lookup, return promise
+DashService.prototype.isValidGameId = function(gameId) {
+    for(var g in this._games) {
+        if( g == gameId) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// TODO: replace this with DB lookup, return promise
 DashService.prototype.getGames = function() {
-    return this.games;
+    return this._games;
+};
+
+// TODO: replace this with DB lookup, return promise
+DashService.prototype.getGameReports = function(gameId) {
+    if( this._games.hasOwnProperty(gameId) &&
+        this._games[gameId].hasOwnProperty('info') &&
+        this._games[gameId].info.hasOwnProperty('reports') ) {
+        return this._games[gameId].info.reports;
+    } else {
+        return [];
+    }
+};
+
+// TODO: replace this with DB lookup, return promise
+DashService.prototype.getGameAchievements = function(gameId) {
+    if( this._games.hasOwnProperty(gameId) &&
+        this._games[gameId].hasOwnProperty('achievements') ) {
+        return this._games[gameId].achievements;
+    } else {
+        return {};
+    }
+};
+
+// TODO: replace this with DB lookup, return promise
+DashService.prototype.getGameDetails = function(gameId) {
+    if( this._games.hasOwnProperty(gameId) &&
+        this._games[gameId].hasOwnProperty('info') &&
+        this._games[gameId].info.hasOwnProperty('details') ) {
+        return this._games[gameId].info.details;
+    } else {
+        return {};
+    }
+};
+
+// TODO: replace this with DB lookup, return promise
+DashService.prototype.getGameMissions = function(gameId) {
+    if( this._games.hasOwnProperty(gameId) &&
+        this._games[gameId].hasOwnProperty('info') &&
+        this._games[gameId].info.hasOwnProperty('missions') ) {
+        return this._games[gameId].info.missions;
+    } else {
+        return null;
+    }
+};
+
+// TODO: replace this with DB lookup, return promise
+DashService.prototype.getGameBasicInfo = function(gameId) {
+    if( this._games.hasOwnProperty(gameId) &&
+        this._games[gameId].hasOwnProperty('info') &&
+        this._games[gameId].info.hasOwnProperty('basic') ) {
+        return this._games[gameId].info.basic;
+    } else {
+        return null;
+    }
+};
+
+// TODO: replace this with DB lookup, return promise
+DashService.prototype.getGameAssessmentInfo = function(gameId) {
+    return this._games[gameId].info.assessment;
 };
 
 
 // TODO: replace this with DB lookup, return promise
-DashService.prototype.getGameAssessmentInfo = function(gameId) {
-    return this.games[gameId].info.assessment;
+DashService.prototype.getGameReportInfo = function(gameId, reportId) {
+    var list = this._games[gameId].info.reports.list;
+    for(var i = 0; i < list.length; i++) {
+        if(list[i].id == reportId) {
+            return list[i];
+        }
+    }
 };
 
 // TODO: replace this with DB lookup
-DashService.prototype._loadGameFiles = function() {
-// add promise wrapper
-return when.promise(function(resolve, reject) {
-// ------------------------------------------------
-    try{
-        var dir = path.join(__dirname, "games");
-        var files = fs.readdirSync(dir);
-
-        files.forEach(function(gameName){
-            // skip dot files
-            if(gameName.charAt(0) != '.') {
-                var gameId = gameName.toUpperCase();
-
-                // gameName is not case sensitive
-                this.games[ gameId ] = {};
-
-                var gameFiles = fs.readdirSync( path.join(dir, gameName) );
-
-                gameFiles.forEach(function(file){
-                    if(file.charAt(0) != '.') {
-                        var name = path.basename(file, path.extname(file));
-                        var filePath = path.join(dir, gameName, file);
-                        this.games[gameId][name] = require(filePath);
-                    }
-                }.bind(this));
-
-                // add developer to game details and reports
-                if( this.games[gameId].info &&
-                    this.games[gameId].info.developer &&
-                    this.games[gameId].info.basic &&
-                    this.games[gameId].info.details &&
-                    this.games[gameId].info.reports ) {
-                    this.games[gameId].info.basic.developer = this.games[gameId].info.developer;
-                }
-
-                // add game info(basic) to game details and reports
-                if( this.games[gameId].info &&
-                    this.games[gameId].info.basic &&
-                    this.games[gameId].info.details &&
-                    this.games[gameId].info.reports ) {
-                    this.games[gameId].info.details = _.merge(this.games[gameId].info.details, this.games[gameId].info.basic);
-                    this.games[gameId].info.reports = _.merge(this.games[gameId].info.reports, this.games[gameId].info.basic);
-                }
-            }
-        }.bind(this));
-    } catch(err) {
-        console.error("DashService: Load Game Files Error -", err);
-    }
-
-    console.log('DashService: Loaded Game Files');
-    resolve();
-// ------------------------------------------------
-}.bind(this));
-// end promise wrapper
-};
-
-
 DashService.prototype.getListOfAchievements = function(gameId, playerAchievement) {
     // if no player achievements then default to none
     if(!playerAchievement) {
@@ -164,17 +181,23 @@ DashService.prototype.getListOfAchievements = function(gameId, playerAchievement
     }
 
     // if not game Id in games, then return empty object
-    if(!this.games.hasOwnProperty(gameId)) {
+    if(!this._games.hasOwnProperty(gameId)) {
         return [];
     }
 
     var achievementsList = [];
-    var a = _.merge(_.cloneDeep(this.games[gameId].achievements), playerAchievement);
+    var a = _.cloneDeep(this._games[gameId].achievements);
     //console.log("a:", a);
 
-    for(var groupId in a.groups) {
-        for(var subGroupId in a.groups[groupId].subGroups) {
-            for(var itemId in a.groups[groupId].subGroups[subGroupId].items) {
+    for(var i = 0; i < a.length; i++) {
+        var groupId = a[i].id;
+
+        for(var j = 0; j < a[i].subGroups.length; j++) {
+            var subGroupId = a[i].subGroups[j].id;
+
+            for(var k = 0; k < a[i].subGroups[j].items.length; k++) {
+                var itemId  = a[i].subGroups[j].items[k].id;
+
                 var achievement = {
                     "group":    groupId,
                     "subGroup": subGroupId,
@@ -182,7 +205,13 @@ DashService.prototype.getListOfAchievements = function(gameId, playerAchievement
                     "won":      false
                 };
 
-                if(a.groups[groupId].subGroups[subGroupId].items[itemId].won) {
+                // playerAchievement stored as tree
+                // get won or not from tree
+                if( playerAchievement.groups &&
+                    playerAchievement.groups.hasOwnProperty(groupId) &&
+                    playerAchievement.groups[groupId].subGroups.hasOwnProperty(subGroupId) &&
+                    playerAchievement.groups[groupId].subGroups[subGroupId].items.hasOwnProperty(itemId) &&
+                    playerAchievement.groups[groupId].subGroups[subGroupId].items[itemId].won) {
                     achievement.won = true;
                 }
 
@@ -192,4 +221,70 @@ DashService.prototype.getListOfAchievements = function(gameId, playerAchievement
     }
 
     return achievementsList;
+};
+
+
+// TODO: replace this with DB lookup
+DashService.prototype._loadGameFiles = function() {
+// add promise wrapper
+    return when.promise(function(resolve, reject) {
+// ------------------------------------------------
+        try{
+            var dir = path.join(__dirname, "games");
+            var files = fs.readdirSync(dir);
+
+            files.forEach(function(gameName){
+                // skip dot files
+                if(gameName.charAt(0) != '.') {
+                    var gameId = gameName.toUpperCase();
+
+                    // gameName is not case sensitive
+                    this._games[ gameId ] = {};
+
+                    var gameFiles = fs.readdirSync( path.join(dir, gameName) );
+
+                    gameFiles.forEach(function(file){
+                        if(file.charAt(0) != '.') {
+                            var name = path.basename(file, path.extname(file));
+                            var filePath = path.join(dir, gameName, file);
+                            this._games[gameId][name] = require(filePath);
+                        }
+                    }.bind(this));
+
+                    // add developer to game details and reports
+                    if( this._games[gameId].info &&
+                        this._games[gameId].info.developer &&
+                        this._games[gameId].info.basic &&
+                        this._games[gameId].info.details &&
+                        this._games[gameId].info.reports ) {
+                        this._games[gameId].info.basic.developer = this._games[gameId].info.developer;
+                    }
+
+                    // add game info(basic) to game details and reports
+                    if( this._games[gameId].info &&
+                        this._games[gameId].info.basic &&
+                        this._games[gameId].info.details &&
+                        this._games[gameId].info.reports ) {
+                        this._games[gameId].info.details = _.merge(this._games[gameId].info.details, this._games[gameId].info.basic);
+                        this._games[gameId].info.reports = _.merge(this._games[gameId].info.reports, this._games[gameId].info.basic);
+                    }
+
+                    var list = this._games[gameId].info.reports.list;
+                    // add achievements to 'achievements' reports
+                    for(var i = 0; i < list.length; i++) {
+                        if(list[i].id == 'achievements') {
+                            this._games[gameId].info.reports.list[i].achievements = this.getGameAchievements(gameId);
+                        }
+                    }
+                }
+            }.bind(this));
+        } catch(err) {
+            console.error("DashService: Load Game Files Error -", err);
+        }
+
+        console.log('DashService: Loaded Game Files');
+        resolve();
+// ------------------------------------------------
+    }.bind(this));
+// end promise wrapper
 };
