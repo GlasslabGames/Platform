54438df25fa306b95e57fc024247fd0e7309d587
Michael Mulligan <michael.mulligan@instituteofplay.org>
2014-08-22 09:12:32 -0700

    - initial supertest commit

--- servers/supertest/apiTest.js
+++ servers/supertest/apiTest.js
@@ -1,2 +1,142 @@
-var testData = require('./lib/testData.js'),
-    supertest = require('supertest');
+var data      = require('./lib/testData.js').stage,
+		routes		= require('./lib/routes.js'),
+		request   = require('superagent'),
+    mocha     = require('mocha'),
+    chai      = require('chai'),
+    expect    = chai.expect;
+
+///////////////////
+// CONFIGURATION //
+///////////////////
+
+var testENV = "stage",
+    srvAddr = data.serverAddress;
+
+var newClassId = 0;
+var agent = request.agent();
+
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+
+function zfill(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function tstamp() {
+	var date = (new Date());
+	return date.getYear() + zfill(date.getMonth(),2) + zfill(date.getDate(),2) + '-' + zfill(date.getHours(),2) + '.' + date.getMinutes() + '.' + date.getSeconds()
+}
+
+
+describe("API v2 testing", function(done) {
+	
+    it("#should connect to " + testENV + "environment", function(done) {
+        agent
+					.get(srvAddr)
+					.end(function(res){
+            expect(res.status).to.eql(200);
+						done();
+					});
+    });
+
+    it("#shouldn't be able to manipulate data without login", function (done) {
+        agent
+					.get(srvAddr + routes.classes.list.path)
+          .type('application/json')
+					.end(function(res){
+            expect(res.status).to.eql(401);
+						done();
+					});
+    });
+    
+    it("#should log in successfully", function(done) {
+
+        agent
+					.post(srvAddr + routes.login.path)
+          .type('application/json')
+          .send(routes.login.post)
+					.end(function(res){
+            expect(res.status).to.eql(200);
+            expect(res.body.id).to.eql(data.teacher.userId);
+						done();
+					});
+        
+    });
+    
+    it("#returns classes correctly", function(done) {
+        
+        agent
+					.get(srvAddr + routes.classes.list.path)
+          .type('application/json')
+					.end(function(res){
+            expect(res.status).to.eql(200);
+						done();
+					});
+        
+    });
+    
+    it.skip("#creates new class", function(done) {
+        
+        // NOTE - adds timestamp to class name
+        var postData = routes.classes.create.post;
+        postData['title'] = postData['title'] + tstamp();
+        
+        agent
+					.post(srvAddr + routes.classes.create.path)
+          .type('application/json')
+          .send(JSON.stringify(postData))
+					.end(function(res){
+            expect(res.status).to.eql(200);
+            newClasId = res.body.id;    // NOTE - passes id, prereq for archive steps
+						done();
+					});
+    });
+    
+    it.skip("#archives new class", function(done) {
+        
+        agent
+					.post(srvAddr + routes.classes.create.path)
+          .type('application/json')
+          .send(JSON.stringify(postData))
+					.end(function(res){
+            expect(res.status).to.eql(200);
+						done();
+					});
+    });
+
+    it.skip("#restores class", function(done) {
+        
+        agent
+					.post(srvAddr + routes.classes.create.path)
+          .type('application/json')
+          .send(JSON.stringify(postData))
+					.end(function(res){
+            expect(res.status).to.eql(200);
+						done();
+					});
+    });
+    
+    it.skip("#returns reports - SOWO", function() {
+        
+    });
+    
+    it.skip("#returns reports - achievements", function() {
+        
+    });
+    
+    it("should log out afterwards", function(done) {
+        
+        agent
+					.post(srvAddr + routes.logout.path)
+          .type('application/json')
+          .send(routes.logout.post)
+					.end(function(res){
+            expect(res.status).to.eql(200);
+						done();
+					});
+    });
+    
+});
--- servers/supertest/lib/testData.js
+++ servers/supertest/lib/testData.js
@@ -1,35 +1,43 @@
-var servers = {
-	local: 'http://localhost:8001',	// FIXME - port hardcode warning
-	stage: "http://stage.playfully.org",
-	prod: "http://playfully.org"
-}
-
 module.exports = {
-	
-	// e2e global server config
-	serverAddress: servers.stage,
 
-	user: {
-		teacher: "build+teach@glasslabgames.org",
-		student: "gl-test01"
-	},
-	classCode: {
-		localhost: 'PQ7N2',
-		stage: 'RJXYL'
-	},
-	classes: {
-		localhost: [
-			'AA-1',
-			'SCE'
-		],
-		stage: [
-			'AA-1',
-			'SCE'
-		]
-	},
-	pass: {
-		teacher: "glasslab123",
-		student: "glasslab321",
-	}
-	
+    stage: {
+        // Stem for supertest
+        serverAddress: 'http://stage.playfully.org',
+        
+        // gameId for MGO
+        testGameId: 'AA-1',
+        
+        // Template for new MGO class
+        newMGOClass: {"title":"newClass","grade":"6, 10, 8","games":[{
+            "gameId":"AA-1","enabled":true,"shortName":"Mars Generation One", "longName":"Mars Generation One - Argubot Academy",
+            "description":"Put your powers of persuasion to the ultimate test, on a whole new planet! Argubot Academy is an adventure game for iOS tablets. Designed for students in grades 6-8, the game develops persuasion and reasoning skills for STEM &amp; 21st century careers.",
+            "settings":{},"license":{"type":"free","valid":true},"thumbnail":{"small":"assets/thumb-game-AA-1.png","large":"assets/thumb-game-AA-1.png"},
+            "developer":{"id":"GL","name":"GlassLab, Inc.","logo":{"small":"assets/glasslab-logo.png","large":"assets/glasslab-logo-2x.png"}},"id":"AA-1"}]
+        },
+        
+        // Manually generated test user
+        teacher: {
+            userId: 6689,
+            email: 'build+teach@glasslabgames.org',
+            pass:  'glasslab123',
+            testClass: {
+                id: 363,
+                dateCreated: 1408059337,
+                // changeables below, not robust test data
+                title: "GLTESTCLASS",
+                grade: "7, 11",
+                code: "ZTLLG"
+            }
+        },
+        student: {}
+    },
+    
+    local: {
+        serverAddress: 'http://localhost:8001', // FIXME - port hardcode warning
+        classCode: 'PQ7N2',
+    },
+    
+    prod: {
+        serverAddress: 'http://playfully.org',
+    }
 }
