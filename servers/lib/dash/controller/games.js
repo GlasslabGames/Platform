3f488c1653c645b122b75084331583c983aeb9f0
Joe <joe@jstty.com>
2014-08-25 01:23:39 -0700

    removed GL_GAME_COURSE_MAP from mysql db in favor of CB for backwards compatibility added auto migration to add games

--- servers/lib/dash/controller/games.js
+++ servers/lib/dash/controller/games.js
@@ -10,6 +10,7 @@ module.exports = {
 };
 
 var exampleIn = {};
+var exampleOut = {};
 
 // no input
 function getGamesBasicInfo(req, res){
@@ -49,21 +50,87 @@ function getGamesBasicInfo(req, res){
     }
 }
 
-function getMyGames(req, res, next) {
+
+// http://localhost:8001/api/v2/dash/myGames
+// returns list of games a user has added to there classes
+// 1) get list of all classes for this user
+// 2) for each class get list of games
+// 3) use full list of games to build set of distinct
+exampleOut.getMyGames = [
+    {
+        gameId: "SC",
+        enabled: true,
+        maintenance: {
+            message: "Coming Soon!"
+        },
+        shortName: "SimCityEdu",
+        longName: "SimCityEDU: Pollution Challenge!",
+        description: "SimCityEDU: Pollution Challenge!",
+        settings: {
+            missionProgressLock: false
+        },
+        license: {
+            type: "tier",
+            valid: false
+        },
+        thumbnail: {
+            small: "assets/thumb-game-SC.png",
+            large: "assets/thumb-game-SC.png"
+        },
+        developer: {
+            id: "GL",
+            name: "GlassLab, Inc.",
+            logo: {
+                small: "assets/glasslab-logo.png",
+                large: "assets/glasslab-logo-2x.png"
+            }
+        }
+    },
+    {
+        gameId: "AA-1",
+        enabled: true,
+        shortName: "Mars Generation One",
+        longName: "Mars Generation One - Argubot Academy",
+        description: "Put your powers of persuasion to the ultimate test, on a whole new planet! Argubot Academy is an adventure game for iOS tablets. Designed for students in grades 6-8, the game develops persuasion and reasoning skills for STEM &amp; 21st century careers.",
+        settings: { },
+        license: {
+            type: "free",
+            valid: true
+        },
+        thumbnail: {
+            small: "assets/thumb-game-AA-1.png",
+            large: "assets/thumb-game-AA-1.png"
+        },
+        developer: {
+            id: "GL",
+            name: "GlassLab, Inc.",
+            logo: {
+                small: "assets/glasslab-logo.png",
+                large: "assets/glasslab-logo-2x.png"
+            }
+        }
+    }
+];
+function getMyGames(req, res) {
     try {
         var userData = req.session.passport.user;
 
-        this.dashStore.getDistinctGames(userData.id)
-            .then(function(gameIdList) {
-
-                for(var i = 0; i < gameIdList.length; i++) {
-                    var gameId = gameIdList[i];
+        // 1) get list of all classes for this user
+        this.lmsStore.getCourseIdsFromUserId(userData.id)
+            .then(function(courseIds) {
+                // 3) use full list of games to build set of distinct
+                return this.telmStore.multiGetDistinctGamesForCourses( courseIds );
+            }.bind(this))
+            // distinct games
+            .then(function(games) {
+                var gamesList = [];
+                for(var gameId in games) {
 
                     // TODO: replace with promise
-                    gameIdList[i] = this.getGameBasicInfo(gameId);
+                    gamesList.push( this.getGameBasicInfo(gameId) );
                 }
 
-                this.requestUtil.jsonResponse(res, gameIdList);
+                this.requestUtil.jsonResponse(res, gamesList);
             }.bind(this))
 
             // catch all errors
