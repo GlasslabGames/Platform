ec75afc22ad21e68e1d5e8424befc728bfc6243b
Michael Mulligan <michael.mulligan@instituteofplay.org>
2014-09-05 13:51:42 -0700

    - allowed git-describe failure to not fail grunt build, for Jenkins

--- servers/Gruntfile.js
+++ servers/Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
             options: {
                 cwd:       ".",
                 commitish: "master",
-                failOnError: true,
+                failOnError: false,
                 template: "{%=tag%}-{%=since%}-{%=object%}{%=dirty%}",
                 prop: 'meta.revision'
             },
