8e6fe7eba691080a6e7d1c5dd7e40114d42dfbee
Joe <joe@jstty.com>
2014-09-18 12:59:29 -0700

    added data for student dash play/download button

--- servers/lib/routes.external.map.js
+++ servers/lib/routes.external.map.js
@@ -191,6 +191,22 @@ module.exports = {
             }
         },
         {
+            api: "/api/v2/dash/games/minimal",
+            service: "dash",
+            controller: "games",
+            method: {
+                get: "getGamesBasicInfo"
+            }
+        },
+        {
+            api: "/api/v2/dash/games/details",
+            service: "dash",
+            controller: "games",
+            method: {
+                get: "getGamesDetails"
+            }
+        },
+        {
             api: "/api/v2/dash/games",
             service: "dash",
             controller: "games",
