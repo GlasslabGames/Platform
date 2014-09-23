9c5d1e9bd455fdf4fa5760d663ecd034c40e3174
Joe <joe@jstty.com>
2014-09-23 00:39:20 -0700

    added gameId check on all events added gameId check on config added default config for all games added auto config creation for new game configs removed an old api function

--- servers/lib/data/controller/config.js
+++ servers/lib/data/controller/config.js
@@ -1,38 +1,71 @@
 
-var Util = require('../../core/util.js');
+var when = require('when');
 var _    = require('lodash');
+
+var Util = require('../../core/util.js');
 
 module.exports = {
     index: index,
     connect: connect
 };
 
-function index(req, res, next)
+// http://127.0.0.1:8001/api/v2/data/config/SC
+//
+function index(req, res, next, serviceManager)
 {
-    // default simcity
-    var gameId = 'SC';
+    gameId = "SC";
     if( req.params &&
         req.params.hasOwnProperty("gameId") ) {
         gameId = req.params.gameId;
     }
 
-    this.cbds.getConfigs(gameId)
-        .then(function(config){
+    // TODO: replace this with DB lookup, return promise
+    if( !serviceManager.get("dash").service.isValidGameId(gameId) ) {
+        this.requestUtil.errorResponse(res, {error: "invalid gameId"});
+        return
+    }
+
+    var defaultConfig = {
+        "eventsMaxSize": 100,
+        "eventsMinSize": 5,
+        "eventsPeriodSecs": 30,
+        "eventsDetailLevel": 10
+    };
+    var configP = [];
+
+    configP.push(
+        this.cbds.getConfigs("default")
+            .then(null, function(err){
+                if(err.code == 13) {
+                    return this.cbds.updateConfigs("default", defaultConfig);
+                }
+                else { return when.reject(err); }
+            }.bind(this))
+    );
+    configP.push(
+        this.cbds.getConfigs(gameId)
+            .then(null, function(err){
+            if(err.code == 13) {
+                return this.cbds.updateConfigs(gameId, defaultConfig);
+            }
+            else { return when.reject(err); }
+        }.bind(this))
+    );
+
+    var p = when.reduce(configP, function(pconfig, config){
+        return _.merge(pconfig, config);
+    }.bind(this), defaultConfig);
+
+    p.then(function(config){
             this.requestUtil.jsonResponse(res, config);
         }.bind(this))
         .then(null, function(err){
-            if(err.code == 13) {
-                this.myds.getConfigs()
-                    .then(function(config) {
-                        this.requestUtil.jsonResponse(res, config);
-                    }.bind(this));
-            } else {
-                console.error("Data Service: Get Config Error -", err);
-                this.requestUtil.errorResponse(res, {key: "data.config.error", statusCode: 500});
-            }
+            console.error("Data Service: Get Config Error -", err);
+            this.requestUtil.errorResponse(res, {key: "data.config.error", statusCode: 500});
         }.bind(this));
 }
 
+// http://127.0.0.1:8001/sdk/connect
 function connect(req, res, next)
 {
     var host = req.headers.host;
