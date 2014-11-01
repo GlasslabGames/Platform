e8326adaf543a72a253636075fb694d3b4efc0ef
Ben Dapkiewicz <ben@glasslabgames.org>
2014-11-01 14:47:39 -0700

    added getAssessmentResults for new netica assessment module posteriors are now calculated and stored as part of the assessment results

--- servers/lib/dash/controller/_game.js
+++ servers/lib/dash/controller/_game.js
@@ -6,6 +6,7 @@ var Util      = require('../../core/util.js');
 
 module.exports = {
     getAssessmentDefinitions: getAssessmentDefinitions,
+    getAssessmentResults:     getAssessmentResults,
     saveAssessmentResults:    saveAssessmentResults
 };
 
@@ -36,14 +37,70 @@ function getAssessmentDefinitions(req, res){
 
         this.requestUtil.jsonResponse(res, this.getGameAssessmentInfo(gameId));
     } catch(err) {
-        console.trace("Reports: Get Assessment Error -", err);
-        this.stats.increment("error", "GetAchievements.Catch");
+        console.trace("Reports: Get Assessment Definitions Error -", err);
+        this.stats.increment("error", "GetAssessmentDefinitions.Catch");
+    }
+}
+
+
+exampleIn.getAssessmentResults = {
+    userId: 47,
+    gameId: 'AA-1',
+    assessmentId: "competency"
+};
+function getAssessmentResults(req, res){
+    try {
+        // check input
+        if( !( req.params &&
+            req.params.hasOwnProperty("gameId") ) ) {
+            this.requestUtil.errorResponse(res, {key:"report.gameId.missing", error: "missing gameId"});
+            return;
+        }
+        var gameId = req.params.gameId;
+        // gameIds are not case sensitive
+        gameId = gameId.toUpperCase();
+
+        // check gameId exists
+        if(!this.isValidGameId(gameId)) {
+            this.requestUtil.errorResponse(res, {key:"report.gameId.invalid", error: "invalid gameId"});
+            return;
+        }
+
+        if( !req.params.userId ) {
+            this.requestUtil.errorResponse(res, {error: "missing userId"});
+            return;
+        }
+        var userId = req.params.userId;
+
+        if( !req.params.assessmentId ) {
+            this.requestUtil.errorResponse(res, {error: "missing assessmentId"});
+            return;
+        }
+        var assessmentId = req.params.assessmentId;
+
+        // get the assessment results for the user
+        this.telmStore.getAssessmentResults(userId, gameId, assessmentId)
+            .then(function(aeResults){
+                //console.log( "getAssessmentResults: ", aeResults );
+                this.requestUtil.jsonResponse(res, aeResults);
+            }.bind(this) )
+            // error
+            .then(null, function(err){
+                this.requestUtil.errorResponse(res, err);
+            }.bind(this) );
+
+    } catch(err) {
+        console.trace("Reports: Get Assessment Results Error -", err);
+        this.stats.increment("error", "GetAssessmentResults.Catch");
     }
 }
 
 
 exampleIn.saveAssessmentResults = {
-
+    userId: 47,
+    gameId: 'AA-1',
+    assessmentId: "competency",
+    data: {}
 };
 function saveAssessmentResults(req, res){
     try {
@@ -120,7 +177,7 @@ function saveAssessmentResults(req, res){
             }.bind(this) );
 
     } catch(err) {
-        console.trace("Reports: Save Assessment Error -", err);
-        this.stats.increment("error", "SaveAssessment.Catch");
+        console.trace("Reports: Save Assessment Results Error -", err);
+        this.stats.increment("error", "SaveAssessmentResults.Catch");
     }
 }
