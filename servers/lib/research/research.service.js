8fa4184b1b32b58c9fba400c58fa870cd1d8dd99
Luke <luke@glasslabgames.org>
2014-11-17 14:33:05 -0800

    create s3 rest api methods and integrate cron job into research.service

--- servers/lib/research/research.service.js
+++ servers/lib/research/research.service.js
@@ -12,13 +12,14 @@ var path      = require('path');
 var _         = require('lodash');
 var when      = require('when');
 var csv       = require('csv');
+var CronJob   = require('cron').CronJob;
 
 // load at runtime
 var Util;
 
 module.exports = ResearchService;
 
-function ResearchService(options){
+function ResearchService(options, serviceManager){
     try {
         var Research;
 
@@ -37,7 +38,8 @@ function ResearchService(options){
         this.requestUtil = new Util.Request(this.options);
         this.store       = new Research.Datastore.Couchbase(this.options.research.datastore.couchbase);
         this.stats       = new Util.Stats(this.options, "Research");
-
+        this._serviceManager = serviceManager;
+        
     } catch(err) {
         console.trace("Auth: Error -", err);
         this.stats.increment("error", "Generic");
@@ -63,6 +65,7 @@ return when.promise(function(resolve, reject) {
         .then(function(){
             // load csv file
 
+            //this.cronJob();
             var dir = __dirname+'/parser_schema/';
             //console.log("dir:", dir);
             fs.readdir(dir, function(error, files) {
@@ -95,4 +98,24 @@ return when.promise(function(resolve, reject) {
 // ------------------------------------------------
 }.bind(this));
 // end promise wrapper
+};
+
+ResearchService.prototype.cronJob = function(){
+    var someCrazyRandomNumber = 0;
+    // actual time wanted: new CronJob('0 0 1 * * *', function(){
+    // will alter this time for prototyping
+    new CronJob('30 30 14 * * *', function(){
+        var a = Date.now();
+        function timeStop(){
+            var b = Date.now();
+            if(b-a < 100){
+                // invoke with properly credentialed req, res, next
+                // exactly how it fits in require more investigation
+                //this.serviceManager.internalRoute('/api/v2/research/game/SC/events', 'get', [req, res, next]);
+                console.log("time test",++someCrazyRandomNumber);
+                setTimeout(timeStop, 10);
+            }
+        }
+        timeStop();
+    }, null, true, "America/Los_Angeles");
 };
