5d3701b6af0376bb575c7451fe00d1626ab2878b
Ben Dapkiewicz <ben@glasslabgames.org>
2015-01-06 08:16:59 -0800

    added learning events for Slice Fractions updated the Slice Fractions info.json with stubs fixed a bug where achievement objects were being incremented incorrectly

--- servers/lib/dash/dash.service.js
+++ servers/lib/dash/dash.service.js
@@ -617,10 +617,9 @@ DashService.prototype._oldLoadGameFiles = function() {
                             for(var i = 0; i < list.length; i++) {
                                 if( _.isObject(list[i]) &&
                                     list[i].id == 'achievements') {
-                                    this._games[gameId].info.reports.list[i].achievements = gameAchievements[index];
+                                    this._games[gameId].info.reports.list[i].achievements = gameAchievements[index++];
                                 }
                             }
-                            index++;
                         }
                     }.bind(this));
 
