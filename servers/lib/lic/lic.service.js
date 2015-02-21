4c55fe059b44232a058e57e7075c82dcadc47887
Luke <luke@glasslabgames.org>
2015-02-20 18:15:53 -0800

    create student api now blocks students from being created into premium classes without student seats

--- servers/lib/lic/lic.service.js
+++ servers/lib/lic/lic.service.js
@@ -352,22 +352,22 @@ LicService.prototype.enrollStudentInPremiumCourse = function(userId, courseId){
         var seats;
         var newStudent;
         this.myds.getLicenseFromPremiumCourse(courseId)
-            .then(function(license){
+            .then(function (license) {
                 licenseId = license.id;
                 seats = license["package_size_tier"];
                 var studentSeatsRemaining = license["student_seats_remaining"];
-                if(studentSeatsRemaining === 0){
+                if (studentSeatsRemaining === 0) {
                     return "lic.students.full";
                 }
                 // get active student list
                 return this.cbds.getActiveStudentsByLicense(licenseId);
             }.bind(this))
-            .then(function(activeStudents){
-                if(activeStudents === "lic.students.full"){
+            .then(function (activeStudents) {
+                if (activeStudents === "lic.students.full") {
                     return activeStudents
                 }
                 newStudent = false;
-                if(activeStudents[userId] === undefined){
+                if (activeStudents[userId] === undefined) {
                     activeStudents[userId] = {};
                     newStudent = true;
                 }
@@ -377,25 +377,25 @@ LicService.prototype.enrollStudentInPremiumCourse = function(userId, courseId){
                 data.students = activeStudents;
                 return this.cbds.updateActiveStudentsByLicense(licenseId, data);
             }.bind(this))
-            .then(function(status){
-                if(status === "lic.students.full"){
+            .then(function (status) {
+                if (status === "lic.students.full") {
                     return status;
                 }
-                if(!newStudent){
+                if (!newStudent) {
                     return;
                 }
                 // if student is a new premium student, update the seat count
                 var studentSeats = lConst.seats[seats].studentSeats;
                 return this.updateStudentSeatsRemaining(licenseId, studentSeats);
             }.bind(this))
-            .then(function(status){
-                if(status === "lic.students.full"){
+            .then(function (status) {
+                if (status === "lic.students.full") {
                     resolve(status);
                     return;
                 }
                 resolve();
             }.bind(this))
-            .then(null, function(err){
+            .then(null, function (err) {
                 console.error("Enroll Student In Premium Course Error -", err);
                 reject(err);
             });
