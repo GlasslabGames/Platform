d2559e3b4bac9ad6703966419cf240eac121e036
Luke <luke@glasslabgames.org>
2015-03-04 08:41:47 -0800

    mend

--- servers/lib/lms/controller/course.js
+++ servers/lib/lms/controller/course.js
@@ -274,13 +274,13 @@ exampleOut.getEnrolledCourses_WithMembers_ForGame =[
                 "id": 175,
                 "lastName": "test2_s1",
                 "firstName": "test2_s1",
-                "username": "test2_s1",
+                "username": "test2_s1"
             },
             {
                 "id": 176,
                 "lastName": "test2_s2",
                 "firstName": "test2_s2",
-                "username": "test2_s2",
+                "username": "test2_s2"
             }
         ]
     }
