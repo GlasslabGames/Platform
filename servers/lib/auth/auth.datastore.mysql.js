2347f049de27654813c398fe03d02e4ed90bd778
Luke <luke@glasslabgames.org>
2015-03-10 17:18:39 -0700

    add paymentType to user object

--- servers/lib/auth/auth.datastore.mysql.js
+++ servers/lib/auth/auth.datastore.mysql.js
@@ -314,6 +314,7 @@ return when.promise(function(resolve, reject) {
                 user.licenseId = license["id"];
                 user.licenseOwnerId = license["user_id"];
                 user.licenseStatus = license["status"];
+                user.paymentType = license["payment_type"];
                 var packageType = license["package_type"];
                 if (packageType === "trial") {
                     user.isTrial = true;
@@ -622,7 +623,7 @@ Auth_MySQL.prototype.getUserEmail = function(userId){
 
 Auth_MySQL.prototype.getLicenseInfoByInstructor = function(userId){
     return when.promise(function(resolve, reject){
-        var Q = "SELECT lic.id,lic.user_id,lic.expiration_date,lm.status FROM GL_LICENSE as lic JOIN\n" +
+        var Q = "SELECT lic.id,lic.user_id,lic.expiration_date,lic.payment_type,lm.status FROM GL_LICENSE as lic JOIN\n" +
             "(SELECT license_id,status FROM GL_LICENSE_MAP\n" +
             "WHERE status in ('active','pending','po-received','po-rejected', 'po-pending') and user_id = " + userId+ ") as lm\n" +
             "ON lic.id = lm.license_id;";
