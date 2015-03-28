436e263fb18c1d0ca44cff203eb746677ae09e6e
Ben Dapkiewicz <ben@glasslabgames.org>
2015-03-28 06:42:19 -0700

    console logs

--- servers/lib/dash/dash.service.js
+++ servers/lib/dash/dash.service.js
@@ -435,9 +435,10 @@ DashService.prototype._migrateGameFiles = function(forceMigrate) {
 // couchbase logic contained in this function, building of _games abstracted to _buildGamesObject
 DashService.prototype._loadGameFiles = function(){
     return when.promise(function(resolve, reject){
-
+        console.log( "loadgamefiles" );
         this.telmStore.getAllGameInformationAndGameAchievements()
             .then(function(results){
+                console.log( "success with load game files: " + results );
                 var ids;
                 var type;
                 var gameId;
@@ -473,12 +474,13 @@ DashService.prototype._loadGameFiles = function(){
 DashService.prototype._buildGamesObject = function(gameInformation, gameAchievements){
     return when.promise(function(resolve, reject){
         try {
+            console.log( "build games object" );
             var gameId;
             var index = 0;
             var achievements = [];
             var gameIds = [];
             _.forEach(gameInformation, function (data, gameId) {
-
+                console.log( "in a game"  + gameId);
                 gameIds.push(gameId);
                 this._games[gameId] = {};
                 if(gameAchievements[gameId] !== undefined){
