99615445c758ec90a76b919d25da52360019d11f
Ben Dapkiewicz <ben@glasslabgames.org>
2015-04-11 20:12:06 -0700

    prima 14 pointing to stage

--- servers/lib/core/util.stats.js
+++ servers/lib/core/util.stats.js
@@ -25,11 +25,11 @@ function Stats(options, root){
     );
 
     // disabled for now
-    statsdInst = new StatsD(this.options.statsd);
+    /*statsdInst = new StatsD(this.options.statsd);
     statsdInst.socket.on('error', function(err) {
         this.statsd = null;
         return console.error("StatsD: Error connecting to server. ", err);
-    });
+    });*/
 
     this.sRoot = root;
     this.root  = root;
