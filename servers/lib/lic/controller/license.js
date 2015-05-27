c415e2c7e1145e4c6ee322571b2a41f0875482ab
Michal <Michal@Michals-Mac-Pro.local>
2015-05-27 13:07:43 -0700

    Revert "GLAS-147 Allow '+' in email addresses in production as they are valid characters for an email address."
    This reverts commit c4bb2a62b36f917ecdb40d24378aa4148063b1ca.

--- servers/lib/lic/controller/license.js
+++ servers/lib/lic/controller/license.js
@@ -499,6 +499,11 @@ function subscribeToTrialLicense(req, res){
         this.requestUtil.errorResponse(res, {key: "lic.create.denied"});
         return;
     }
+    var emailDomain = req.user.email.split('@')[1].indexOf('glasslabgames.org');
+    if(this.options.env === "prod" && req.user.email.indexOf("+") !== -1 && emailDomain !== -1){
+        this.requestUtil.errorResponse(res, {key: "lic.email.invalid"});
+        return;
+    }
     var userId = req.user.id;
     var stripeInfo = {};
     var planInfo = {
