29602cc88b0c77e02ab355008eee32dee4fb27f6
wcj <wcj@makingwonder.com>
2015-07-20 09:37:15 -0700

    Stats.gauge() etc

--- servers/lib/core/util.stats.js
+++ servers/lib/core/util.stats.js
@@ -31,7 +31,7 @@ function Stats(options, root){
 
     this.sRoot = root;
     this.root  = root;
-    this.env   = process.env.HYDRA_ENV || 'dev';
+    this.env   = process.env.HYDRA_ENV || 'dev';    // env = 'prod', 'stage', or 'dev'
 }
 
 Stats.prototype.saveRoot = function() {
@@ -54,16 +54,16 @@ Stats.prototype.increment = function(level, key, count) {
     if(statsdInst) {
         level = level.toLowerCase();
 
-        // // dev.App.error.Loaded
-        // statsdInst.increment(this.env+"."+this.root+"."+level+"."+key, count);
+    //  statsdInst.increment(this.env+"."+this.root+"."+level+"."+key, count);      // dev.App.error.Loaded
 
-        // dev.error.App.Loaded
-        statsdInst.increment(this.env+"."+level+"."+this.root+"."+key, count);
+        statsdInst.increment(this.env+"."+level+"."+this.root+"."+key, count);      // dev.error.App.Loaded
+    //  statsdInst.increment("EnvALL."+level+"."+this.root+"."+key, count);         // EnvALL.error.App.Loaded
+    //  statsdInst.increment(level+"."+this.root+"."+key, count);                   // error.App.Loaded
 
-        if(-1 < level.indexOf("err")){
+//      if(-1 < level.indexOf("err")){
             statsdInst.increment(this.env+"."+level+"."+this.root+"._total", count);    // dev.error.App._total
             statsdInst.increment(this.env+"."+level+"._total", count);                  // dev.error._total
-        }
+//      }
     }
 };
 
@@ -71,7 +71,9 @@ Stats.prototype.set = function(level, key, value) {
 
     if(statsdInst) {
         level = level.toLowerCase();
-        statsdInst.set(this.env+"."+level+"."+this.root+"."+key, value);
+        statsdInst.set(this.env+"."+level+"."+this.root+"."+key, value);    // stage.info.ServiceManager.server_started_any
+    //  statsdInst.set("EnvALL."+level+"."+this.root+"."+key, value);       // EnvALL.info.ServiceManager.();
+    //  statsdInst.set(level+"."+this.root+"."+key, value);                 // info.ServiceManager.();
     }
 };
 
@@ -79,8 +81,8 @@ Stats.prototype.gauge = function(level, key, value) {
 
     if(statsdInst) {
         level = level.toLowerCase();
-
-        // dev.error.App.Loaded
-        statsdInst.gauge(this.env+"."+level+"."+this.root+"."+key, value);
+        statsdInst.gauge(this.env+"."+level+"."+this.root+"."+key, value);      // prod.info.App.Loaded
+    //  statsdInst.gauge("EnvALL."+level+"."+this.root+"."+key, value);         // EnvALL.info.App.Loaded
+    //  statsdInst.gauge(level+"."+this.root+"."+key, value);                   // info.App.Loaded
     }
 };
