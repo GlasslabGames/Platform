0852a3709ade45cb097eef5cc0fc604c59904d04
Kenneth Yeast <kenneth.yeast@gmail.com>
2015-08-21 18:17:29 -0700

    Fix for commit: b4882d5 (multi-instructor accounts would cause exception)

--- servers/lib/auth/auth.datastore.mysql.js
+++ servers/lib/auth/auth.datastore.mysql.js
@@ -297,11 +297,11 @@ return when.promise(function(resolve, reject) {
 
                 // if input not array then return a single user
                 if(!_.isArray(value)) {
-                    user = user[0];
-                    if(user["verifyCodeStatus"] === "invited"){
+                    single_user = user[0];
+                    if(single_user["verifyCodeStatus"] === "invited"){
                         return "tempUser";
-                    } else if(user.role === "instructor"){
-                        return this.getLicenseRecordsByInstructor(user.id);
+                    } else if(single_user.role === "instructor"){
+                        return this.getLicenseRecordsByInstructor(single_user.id);
                     }
                 }
                 return [];
@@ -313,9 +313,9 @@ return when.promise(function(resolve, reject) {
             if(!((results === "none")||(results.length===0))){
                 // any license results are sufficient for "hadTrial" (I believe if they paid and expired, they cannot get a trial.  IF this is not true, we'll might need to add a column to track trial usage after all.)
                 user.hadTrial = true;
-                console.log("user had trial or previous subscriptions: ", user.username);
+                console.log("user had trial or previous subscriptions: ", single_user.username);
             }
-            return this.getLicenseInfoByInstructor(user.id);
+            return this.getLicenseInfoByInstructor(single_user.id);
         }.bind(this))
         .then(function(results){
             if(typeof results === "string"){
@@ -324,11 +324,11 @@ return when.promise(function(resolve, reject) {
             }
             // if gained results, add all relevant license information
             if(results.length > 0) {
-                return _addLicenseInfoToUser.call(this, user, results);
+                return _addLicenseInfoToUser.call(this, single_user, results);
             }
         }.bind(this))
         .then(function(){
-            resolve(user);
+            resolve(single_user);
         })
         .then(null, function(err) {
             reject({"error": "failure", "exception": err}, 500);
