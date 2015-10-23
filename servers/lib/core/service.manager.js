fd2d2f200b4bd704748a680505e46a8c68b4b39e
wcj <wcj@makingwonder.com>
2015-10-23 00:02:40 -0700

    express 4.0 bodyParser.urlencoded() requires explicit extended value

--- servers/lib/core/service.manager.js
+++ servers/lib/core/service.manager.js
@@ -285,7 +285,7 @@ return when.promise(function(resolve, reject) {
               this.app.use(function (req, res, next) { console.log("no-op"); next(); });
               
                 this.app.use(cookieParser());
-                this.app.use(bodyParser.urlencoded({limit: "500kb"}));
+                this.app.use(bodyParser.urlencoded({limit: "500kb", extended: false}));
                 this.app.use(bodyParser.json({limit: "500kb"}));
                 this.app.use(methodOverride());
                 
