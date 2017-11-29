d920f9d135630fba7b9204214ddc2b2e87a9d34a
Louis Byers <louis@concentricsky.com>
2017-11-29 15:07:02 -0800

    GLAS-511 only making changes to stripe plan ids

--- servers/lib/lic/lic.const.js
+++ servers/lib/lic/lic.const.js
@@ -16,28 +16,28 @@ module.exports = {
     },
     "plan": {
         "chromebook": {
-            "stripe_planId": "GLG_CHROMEBOOK_P03",
+            "stripe_planId": "GLG_CHROMEBOOK_P03_0",
             "name": "Chromebook/Web",
             "description": "Games in this package are optimized to work on Chromebooks.",
             "pricePerSeat": 2.5,
             "planId": "chromebook"
         },
         "ipad": {
-            "stripe_planId": "GLG_IPAD_P04",
+            "stripe_planId": "GLG_IPAD_P04_0",
             "name": "iPad",
             "description": "Games in this package are optimized to work on iPads.",
             "pricePerSeat": 3,
             "planId": "ipad"
         },
         "pcMac": {
-            "stripe_planId": "GLG_PCMAC_P05",
+            "stripe_planId": "GLG_PCMAC_P05_0",
             "name": "PC/MAC",
             "description": "Games in this package run on PC or Mac computers.",
             "pricePerSeat": 3.5,
             "planId": "pcMac"
         },
         "allGames": {
-            "stripe_planId": "GLG_ALLGAMES_P06",
+            "stripe_planId": "GLG_ALLGAMES_P06_0",
             "name": "All Games",
             "description": "This package includes all games available on this site.",
             "pricePerSeat": 5,
